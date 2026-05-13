7ea565f4464fe34b27aad6f8826252969cc5839f
ncs01060 <ncs01045@gmail.com>
2026-05-13 20:27:33 +0900

    로컬 상태로 데이터 관리하기

--- App.tsx
+++ App.tsx
@@ -1,15 +1,67 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [clickCount, setClickCount] = useState<number>(0);
+
+  const generateColor = (): string => {
+    const randomColor = Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0");
+    return `#${randomColor}`;
+  };
+
+  function SomeComponent(props: any) {
+    return (
+      <>
+        <Text
+          style={[
+            { fontWeight: "bold" },
+            { fontSize: 20 },
+            { backgroundColor: generateColor() },
+          ]}
+        >
+          someComponent
+        </Text>
+        <Text style={[{ fontWeight: "bold" }, { fontSize: 20 }]}>
+          들어온 데이터 {props.clickCount}
+        </Text>
+        <Button title="버튼이다!" onPress={props.onClick} />
+      </>
+    );
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: generateColor() }]}>
       <Text
-        style={[{ fontWeight: "bold" }, { fontSize: 42 }, { color: "darkred" }]}
+        style={[
+          { fontWeight: "bold" },
+          { fontSize: 20 },
+          { backgroundColor: generateColor() },
+        ]}
       >
-        Hello World!
+        오늘도 빡코딩!!
       </Text>
+
+      <Text style={[styles.boldText, { backgroundColor: generateColor() }]}>
+        오늘도 빡코딩!! {clickCount}
+      </Text>
+
       <StatusBar style="auto" />
+      <Text style={styles.boldText}>클릭수! {clickCount}</Text>
+      <Button
+        title="버튼이다!"
+        onPress={(e) => {
+          setClickCount(clickCount + 1);
+        }}
+      />
+      <SomeComponent
+        clickCount={clickCount}
+        onClick={() => {
+          setClickCount(clickCount + 1);
+        }}
+      />
     </View>
   );
 }
@@ -20,5 +72,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  boldText: {
+    fontWeight: "bold",
+    fontSize: 26,
   },
 });
